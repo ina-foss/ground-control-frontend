153f9a3bfa2b0eaad35cc2e4618198ca8fa313eb
mjelassi <mjelassi-prestataire@ina.fr>
2024-05-28 17:08:59 +0200

    grouping of segments/fix war

---
+++ tests/imports.test.ts
@@ -0,0 +1,32 @@
+import {expect, describe, test, it} from 'vitest'
+import { mount } from '@vue/test-utils'
+import Dashboard from '~/pages/dashboard.vue'
+describe('import vue components',() =>{
+
+    it('can mount some component', async () => {
+        const component = mount(Dashboard)
+        expect(component.text()).toContain('')
+    })
+
+    test('Header imports as expected', async ()=>{
+        const cmp = await import('../components/Header.vue')
+        expect(cmp).toBeDefined()
+    })
+    test('DataDialog imports as expected', async ()=>{
+        const cmp = await import('../components/DataDialog.vue')
+        expect(cmp).toBeDefined()
+    })
+    test('SegmentationMolecules imports as expected', async ()=>{
+        const cmp = await import('../components/SegmentationMolecules.vue')
+        expect(cmp).toBeDefined()
+    })
+    test('ProjectCard imports as expected', async ()=>{
+        const cmp = await import('../components/ProjectCard.vue')
+        expect(cmp).toBeDefined()
+    })
+    test('LoadingSpinner imports as expected', async ()=>{
+        const cmp = await import('../components/LoadingSpinner.vue')
+        expect(cmp).toBeDefined()
+    })
+
+})
