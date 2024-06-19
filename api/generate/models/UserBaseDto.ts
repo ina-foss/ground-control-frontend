6e0bb9e83a7900d8de9ee1965b1e044b953a7997
Pablo <pratouit-prestataire@ina.fr>
2024-06-19 18:07:32 +0200

    Check if user exist in db, if not insert it. Might need refacto

---
+++ api/generate/models/UserBaseDto.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * Base DTO representing a user object.
+ */
+export type UserBaseDto = {
+    email: string;
+    role: string;
+};
+
