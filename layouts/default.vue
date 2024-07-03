521437e4a53ca8ff5c6daf21f34f832b69a3d507
Pablo <pratouit-prestataire@ina.fr>
2024-07-03 14:42:28 +0200

    Add footer to layout and fix slot size

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="bg-gray-50 min-h-screen ">
     <Header />
-    <slot />
+    <main class="h-[calc(100vh-52px)]">
+      <slot />
+    </main>
+    <MoleculeFooter />
   </div>
 </template>
 
 
 
 <script setup>
+import MoleculeFooter from '~/components/molecules/MoleculeFooter.vue';
+
 </script>
