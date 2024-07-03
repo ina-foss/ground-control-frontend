0040638ec248c4a91b66399442c4e5e22c78d14b
Pablo <pratouit-prestataire@ina.fr>
2024-07-03 17:56:52 +0200

    fix colormode switch and logout

--- components/atoms/AtomAvatarHeader.vue
+++ components/atoms/AtomAvatarHeader.vue
@@ -12,33 +12,53 @@ v-tooltip.left="userEmail" class="cursor-pointer" icon="pi pi-user" shape="circl
 
 const router = useRouter()
 const authStore = useAuth()
+const authService = useService()
 const { userEmail } = storeToRefs(authStore);
+const colorMode = useColorMode()
 const menu = ref();
-const items = ref([
-    {
-        label: 'Options',
-        items: [
-            {
-                label: 'Refresh token',
-                icon: 'pi pi-refresh',
-                command: () => {
-                    router.push("/silent-refresh")
+const items = computed(() => {
+    const nextColorMode = colorMode.preference === "light" ? 'dark' : 'light'
+
+    return [
+        {
+            label: 'Options',
+            items: [
+                {
+                    label: `Toggle ${nextColorMode} `,
+                    icon: 'pi toggle-icon',
+                    command: () => {
+                        toggleDarkMode()
+                    }
+                },
+                {
+                    label: 'Refresh token',
+                    icon: 'pi pi-refresh',
+                    command: () => {
+                        router.push("/silent-refresh")
+                    }
+                },
+                {
+                    label: 'Logout',
+                    icon: 'pi pi-sign-out',
+                    command: () => {
+                        handleLogout()
+                    }
                 }
-            },
-            {
-                label: 'Logout',
-                icon: 'pi pi-sign-out',
-                command: () => {
-                    handleLogout()
-                }
-            }
-        ]
-    }
-]);
+            ]
+        }
+    ]
+});
 
 const toggle = (event) => {
     menu.value.toggle(event);
 };
+
+
+
+const toggleDarkMode = () => {
+    if (colorMode.preference == 'dark') colorMode.preference = 'light'
+    else colorMode.preference = 'dark'
+}
 
 const handleLogout = () => authService.$auth.logout()
 </script>
