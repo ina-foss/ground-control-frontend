6b670cd563bf5ca9fda73ba326baf5ab3b586ee0
Pablo <pratouit-prestataire@ina.fr>
2024-07-03 15:58:10 +0200

    Rename Header and organize molecules

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-50 min-h-screen ">
-    <Header />
+    <MoleculeHeader />
     <main class="h-[calc(100vh-52px)]">
       <slot />
     </main>
@@ -12,5 +12,6 @@
 
 <script setup>
 import MoleculeFooter from '~/components/molecules/MoleculeFooter.vue';
+import MoleculeHeader from '~/components/molecules/MoleculeHeader.vue';
 
 </script>
