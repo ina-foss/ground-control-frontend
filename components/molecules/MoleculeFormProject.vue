44d22d85b069dd1268997d68cb4da115a1a346e4
Pablo <pratouit-prestataire@ina.fr>
2024-07-17 11:06:57 +0200

    Refacto and project creation

--- components/molecules/MoleculeFormProject.vue
+++ components/molecules/MoleculeFormProject.vue
@@ -1,9 +1,9 @@
 <template>
-  <Dialog v-bind:visible="dialogVisible" modal header="Your Project" :style="{ width: 'fit-content' }" class="bg-white "
-            @hide="$emit('refreshData')" @after-hide="deleteDialog = false" @update:visible="emits('toggle-dialog')">
-    <Stepper class="transition-all" >
+  <Dialog :visible="dialogVisible" modal header="Your Project" :style="{ width: 'fit-content' }" class="bg-white "
+    @hide="$emit('refreshData')" @after-hide="deleteDialog = false" @update:visible="emits('toggle-dialog')">
+    <Stepper class="transition-all">
       <StepperPanel class="transition-all" header="Infos">
-        <template class="transition-all" #content="{ nextCallback }">
+        <template #content="{ nextCallback }" class="transition-all">
           <div class=" grid grid-cols-1 grid-rows-3 gap-3 min-w-[70vh]">
             <span class="text-slate-400 ">Enter project configuration</span>
             <div class="flex grid-cols-2 gap-3 align-items-center ">
@@ -21,11 +21,11 @@
             </div>
             <div class="flex justify-between items-center">
               <label class="self-center text-sm ">Published ?</label>
-              <InputSwitch v-model="isPublished"  class="scale-75" />
+              <InputSwitch v-model="isPublished" class="scale-75" />
               <label class="self-center text-sm">Allow empty annotations ?</label>
               <InputSwitch v-model="emptyAnnotations" class="scale-75" />
               <label class="self-center text-sm">Allow skip ?</label>
-              <InputSwitch v-model="allowSkip" class="scale-75"/>
+              <InputSwitch v-model="allowSkip" class="scale-75" />
             </div>
             <!-- <ButtonGroup class="justify-evenly flex items-center pt-6 "> -->
             <!--   <Button label="Update" severity="info" class="justify-self-center" @click="updateProject" /> -->
@@ -33,49 +33,49 @@
             <!--   <Button v-else label="Sure ?" severity="danger" class="" @click="deleteProject" /> -->
             <!-- </ButtonGroup> -->
           </div>
-          <div class="flex justify-end pt-8" >
-            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" size="small" @click="nextCallback" />
+          <div class="flex justify-end pt-8">
+            <Button label="Next" icon="pi pi-arrow-right" icon-pos="right" size="small" @click="nextCallback" />
           </div>
         </template>
       </StepperPanel>
       <StepperPanel header="Medias">
-        <template #content="{ prevCallback, nextCallback }" >
+        <template #content="{ prevCallback, nextCallback }">
           <div class="flex justify-center min-w-[70vh]">
             <p>CHOIX DES MEDIAS WIP</p>
           </div>
           <div class="flex justify-between pt-8">
-            <Button label="Previous" icon="pi pi-arrow-left" iconPos="right" size="small" @click="prevCallback" />
-            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" size="small" @click="nextCallback" />
+            <Button label="Previous" icon="pi pi-arrow-left" icon-pos="right" size="small" @click="prevCallback" />
+            <Button label="Next" icon="pi pi-arrow-right" icon-pos="right" size="small" @click="nextCallback" />
           </div>
         </template>
       </StepperPanel>
       <StepperPanel header="Steps">
-        <template #content="{ prevCallback, nextCallback }" >
+        <template #content="{ prevCallback, nextCallback }">
           <div class="w-[70vh] grid-cols-3 flex">
             <div class="">
               <label>Available</label>
-              <Listbox :options="availableType" v-model="selectedType" multiple class="basis-1/3" />
+              <Listbox v-model="selectedType" :options="availableType" multiple class="basis-1/3" />
             </div>
-            <span class=" flex justify-center items-center basis-1/3"><i class="pi pi-arrow-right scale-150"/></span>
-            <div class="basis-1/3" >
+            <span class=" flex justify-center items-center basis-1/3"><i class="pi pi-arrow-right scale-150" /></span>
+            <div class="basis-1/3">
               <label>Selected</label>
-              <ol class="border-surface-300 border-[1px] py-3 rounded">
-                <li v-for="(type,index) in selectedType" :key="index" class="leading-none px-5 py-3 self-center overflow-visible">{{index+1}}. {{type}}</li>
+              <ol v-if="selectedType.length != 0" class="border-surface-300 border-[1px] py-3 rounded">
+                <li v-for="(type, index) in selectedType" :key="index"
+                  class="leading-none px-5 py-3 self-center overflow-visible">{{ index + 1 }}. {{ type }}</li>
               </ol>
             </div>
           </div>
           <div class="flex justify-between pt-8">
-            <Button label="Previous" icon="pi pi-arrow-left" iconPos="right" size="small" @click="prevCallback" />
-            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" size="small" @click="nextCallback" />
+            <Button label="Previous" icon="pi pi-arrow-left" icon-pos="right" size="small" @click="prevCallback" />
+            <Button label="Next" icon="pi pi-arrow-right" icon-pos="right" size="small" @click="nextCallback" />
           </div>
         </template>
       </StepperPanel>
       <StepperPanel header="Annotations">
-        <template #content="{ prevCallback, nextCallback }" >
+        <template #content="{ prevCallback, nextCallback }">
           <div class="w-[70vh]">
-            <FileUpload
-      accept="application/json" :show-upload-button=false :show-cancel-button="false"
-               invalid-file-type-message="Invalid type" auto name="file[]" :pt="{
+            <FileUpload accept="application/json" :show-upload-button=false :show-cancel-button="false"
+              invalid-file-type-message="Invalid type" auto name="file[]" :pt="{
                 buttonbar: {
                   style: `z-index:20; padding-top: 10px; padding-bottom: 10px; `
                 },
@@ -83,8 +83,7 @@
                   style: ''
                 },
 
-              }"   @upload="onUpload($event)"
-              @error="onUpload($event)">
+              }" @upload="onUpload($event)" @error="onUpload($event)">
               <template #empty="{ chooseCallback }">
 
                 <div class="flex items-center justify-content-center flex-col">
@@ -95,7 +94,8 @@
 
               <template #content="{ uploadedFiles, files, removeUploadedFileCallback, removeFileCallback }">
                 <div class="flex flex-col gap-2">
-                  <div v-for="(file, index) in uploadedFiles" :key="index" class="grid grid-cols-8 gap-2 px-1 items-center">
+                  <div v-for="(file, index) in uploadedFiles" :key="index"
+                    class="grid grid-cols-8 gap-2 px-1 items-center">
                     <span class="pi pi-file self-center w-2" />
                     <p v-tooltip.top="file.name" class="text-ellipsis text-nowrap col-span-4 overflow-hidden ">
                       {{ file.name }}
@@ -105,8 +105,7 @@
                     <p v-else class="text-slate-400 text-xs text-nowrap col-span-2  ">{{ Math.round(file.size /
                       1024) }} KB
                     </p>
-                    <Button
-      icon="pi pi-times" text rounded size="small" severity="danger"
+                    <Button icon="pi pi-times" text rounded size="small" severity="danger"
                       class=" self-center hover:bg-surface-100  hover:cursor-pointer" style="font-size: 15px;" :pt="{
                         root: {
                           style: 'justify-content: center; justify-items: center; place-self: center;'
@@ -126,62 +125,111 @@
 
           </div>
           <div class="flex justify-between pt-8">
-            <Button label="Previous" icon="pi pi-arrow-left" iconPos="right" size="small" @click="prevCallback" />
-            <Button label="Next" icon="pi pi-arrow-right" iconPos="right" size="small" @click="nextCallback" />
+            <Button label="Previous" icon="pi pi-arrow-left" icon-pos="right" size="small" @click="prevCallback" />
+            <Button label="Create" severity="success" icon="pi pi-check" size="small" @click="createProject" />
           </div>
         </template>
       </StepperPanel>
     </Stepper>
-  </Dialog >
+  </Dialog>
 </template>
 
 <script setup>
 
 
-  import InputSwitch from 'primevue/inputswitch';
-  import { ProjectStatus } from '~/api/generate';
-  import { AnnotationType } from '~/api/generate';
-  console.log(Object.values(AnnotationType))
-  const { dialogVisible, project } = defineProps(['dialogVisible','project'])
+import InputSwitch from 'primevue/inputswitch';
+import { ProjectStatus, AnnotationType } from '~/api/generate';
+import { ProjectService } from '~/api/generate';
+import { useRefreshStore } from '#imports';
+import { useAuth } from '#imports';
 
-  const emits = defineEmits(['toggle-dialog','refreshData'])
-  const visible = computed(()=> dialogVisible )
-  const deleteDialog = $ref(false)
-
-  const title = $ref(project?.title)
-  const description = $ref(project?.description)
-  const status = $ref(project?.status)
-  const isPublished = $ref(project?.is_published)
-  const allowSkip = $ref(project?.allow_skip)
-  const emptyAnnotations = $ref(project?.empty_annotations)
+const { dialogVisible, project } = defineProps(['dialogVisible', 'project'])
 
 
-  const files = $ref([])
-  const fileData = $ref([])
+const emits = defineEmits(['toggle-dialog', 'refreshData'])
+const visible = computed(() => dialogVisible)
+const deleteDialog = $ref(false)
 
-  const availableType = $ref(Object.values(AnnotationType))
-  const selectedType = $ref([])
+const title = $ref(project?.title || '')
+const description = $ref(project?.descriptio || '')
+const status = $ref(project?.status)
+const isPublished = $ref(project?.is_published || false)
+const allowSkip = $ref(project?.allow_skip || false)
+const emptyAnnotations = $ref(project?.empty_annotations || false)
 
 
-  const updateProject = async () => {
-    if (title.value == project.value.title && description.value == project.value.description) {
-    } else {
-      const response = await ProjectService.updateProjectProjectProjectIdPut(project.value.id, {
-        title: title.value,
-        description: description.value,
-        status: project.value.status,
-        annotation_type: project.value.annotation_type,
-        is_published: project.value.is_published,
-        empty_annotations: project.value.empty_annotations,
-        allow_skip: project.value.allow_skip,
-        control_weights: project.value.control_weights,
-        pinned_at: project.value.pinned_at,
-        created_by: "john@example.com",
+let files = $ref([])
+let fileData = $ref([])
+
+const availableType = $ref(Object.values(AnnotationType))
+const selectedType = $ref([])
+
+const refreshStore = useRefreshStore()
+const { userEmail, user } = useAuth()
+const toast = useToast()
+
+
+const updateProject = async () => {
+  if (title.value == project.value.title && description.value == project.value.description) {
+  } else {
+    const response = await ProjectService.updateProjectProjectProjectIdPut(project.value.id, {
+      title: title.value,
+      description: description.value,
+      status: project.value.status,
+      annotation_type: project.value.annotation_type,
+      is_published: project.value.is_published,
+      empty_annotations: project.value.empty_annotations,
+      allow_skip: project.value.allow_skip,
+      control_weights: project.value.control_weights,
+      pinned_at: project.value.pinned_at,
+      created_by: "john@example.com",
+
+    })
+    visible.value = false
+  }
+}
+
+const createProject = async () => {
+  if (title == null) {
+    errorVisible.value = true;
+  }
+  else {
+
+    const response = ProjectService.createProjectProjectPost({
+      title: title,
+      description: description,
+      status: status,
+      is_published: isPublished,
+      empty_annotations: emptyAnnotations,
+      allow_skip: allowSkip,
+      control_weights: 10,
+      pinned_at: null,
+      created_by: userEmail
+    })
+
+    response.catch(() => (toast.add({ severity: 'danger', detail: 'Project could not be created', summary: 'Something went wrong' }))).then((res) =>{
+      // fileData.value.forEach((file, index) => {
+      //   TaskService.createTaskTaskPost({
+      //     name: `Task #${index + 1}`,
+      //     instruction: 'Instruction',
+      //     data: file,
+      //     project_id: res.id
+      //   }).catch((err) => console.error(err)).then((res) => console.log(res))
+      // })
+      // TODO: Implement the step creation by iteratiing throught selectedType
+      selectedType.forEach((type) => {
+        return true
 
       })
-      visible.value = false
-    }
+      files = []
+      fileData = []
+      emits('toggle-dialog')
+
+      refreshStore.fetch()
+
+    })
   }
+}
 
 const deleteProject = async () => {
   await ProjectService.deleteProjectProjectProjectIdDelete(project.value.id)
@@ -216,7 +264,7 @@ const onReaderLoad = (event) => {
 </script>
 
 <style scoped>
-.p-steplist{
+.p-steplist {
   transition: "all 1s ease-in-out"
 }
 </style>
