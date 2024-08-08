3de9fd8b30a8e03667c771c8be50763488d459b4
Pablo <pratouit-prestataire@ina.fr>
2024-08-08 14:12:46 +0200

    Display selected transciption

--- components/atoms/AtomTrancription.vue
+++ components/atoms/AtomTrancription.vue
@@ -1,28 +1,31 @@
 <template>
-  <div ref="test" class="bg-surface-200 flex hover:scale-[1.005] hover:mb-2 hover:mt-[8px]   flex-col p-4 gap-1 drop-shadow-lg rounded transition-all " >
+  <div ref="test" class="bg-surface-200 flex  flex-col p-4 gap-1 drop-shadow-lg rounded transition-all " >
     <div class="flex justify-between pb-2  ">
+      <div class="flex  flex-start gap-2 ">
       <Tag :severity="tcColor">
         <div class="flex justify-center items-center gap-3">
           <i class="pi pi-clock" style="font-size: 1.5rem" />
           <p class="text-sm">{{transcriptions[0].tcin}}</p>
         </div>
       </Tag>
+      <Tag v-if="selectedTranscription.index != null" severity="info" class="h-7 self-center "  :value="transcriptionTag" />
+      </div>
       <Button icon="pi pi-pencil" size="small" severity="contrast" @click="onExpand()"/>
     </div>
     <div v-if="isExpand == true" class="flex flex-col gap-2 " >
       <div  class="flex flex-row gap-3 w-full ">
         <span v-for="(phrase, index) in transcriptions" :key="index" class="rounded bg-gray-500 text-gray-100 relative p-2 scroll-mt-5 ">
-          <Tag severity="secondary" :value="algos[index]" />
+          <Tag severity="secondary" :value="algos[index] " />
           <p>{{ phrase.data.text[0] }}</p>
           <div class="flex justify-end">
            <Button  @click="selectTrancription(phrase, index)" icon="pi pi-check" :outlined="true" rounded style="scale: 0.8;"  severity="success" class=""/>
           </div>
         </span>
       </div>
-      <div class="w-full bg-white flex-col items-center p-2 ">
+      <div class="w-full bg-white flex-col items-center rounded p-2 ">
         <div class="flex justify-between pb-2  ">
           <h2>Result</h2>
-          <Tag v-if="selectedTranscription.index != null" severity="info" :value="algos[selectedTranscription.index]" />
+          <Tag v-if="selectedTranscription.index != null" severity="info" :value="transcriptionTag" />
           <p  >edit</p>
         </div>
         <div class="flex justify-center">
@@ -61,6 +64,14 @@
   let isFinished = $ref(false) // If the transcription has been corrected
   const test = ref()
   const selectedTranscription = reactive({text: '', index: null})
+
+  const isEdited = computed(() => (selectedTranscription.text == '' || selectedTranscription.text == transcriptions[selectedTranscription.index].data.text[0]) ? false : true )
+  const transcriptionTag = computed(() => {
+    const editedTag =  isEdited.value ? 'custom' : ''
+    if (selectedTranscription.index != null ) return algos[selectedTranscription.index]+' '+ editedTag
+    else return ''
+  })
+  watchEffect(()=> console.log(transcriptionTag.value))
 
   const tcColor = computed(()=>{
     return isFinished ?
