8c5d446fd2244a570fa65188ef0c561bdc884cbe
Pablo <pratouit-prestataire@ina.fr>
2024-08-16 14:04:03 +0200

    fix topic list first color

--- components/atoms/AtomTopicList.vue
+++ components/atoms/AtomTopicList.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="flex flex-wrap justify-center gap-3 p-3 ">
-    <!-- WARN: The v-for loop reads the index 0 which represents no topic. Might use computed array without first index -->
-        <div v-for="(color, index) in colors" :key="index">
+        <div v-for="(color, index) in cleanedColors" :key="index">
           <div v-if="index != 0" class="flex h-8 items-center bg-white overflow-visible rounded border-surface-400 border-[1px]" >
             <div :style="`background-color: ${color}`" class="w-8 h-8 ml-[-1px]  rounded-sm flex justify-center z-10 items-center text-white font-bold text-sm ">#{{index}}</div>
             <h2 class="px-2 font-semibold" >Topic</h2>
@@ -18,4 +17,14 @@
 
   const { colors } = defineProps<Props>()
 
+  const cleanedColors = $computed(()=>{ // delete the first element
+    let cleaned = {}
+
+    colors.forEach((color,index)=>{
+      if (index != 0){
+        cleaned[index]= color
+      }
+    })
+    return cleaned
+  })
 </script>
