88bf875df388f567ce55d9fee4bcbebb6b73eae0
Pablo <pratouit-prestataire@ina.fr>
2024-08-19 14:39:09 +0200

    Amalia WIP

---
+++ components/atoms/AtomVideoAmalia.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class=" xs:h-full w-full h-auto xs:flex xs:justify-center ">
+    <div id="myplayer"></div>
+  </div>
+</template>
+
+
+<script setup>
+
+
+const { videoSrc } = defineProps(['videoSrc'])
+
+
+let dymamicSrc = $ref()
+
+async function fetchVideoStream(url) {
+  const response = await fetch(url);
+  const videoHls = response.text();
+
+  return videoHls;
+}
+
+
+  const hlsPlayer = () => {
+    fetchVideoStream(videoSrc).then((content) => {
+      const src = `data:application/vnd.apple.mpegurl;base64,${content}`
+      dymamicSrc = src
+    })
+  }
+
+
+onMounted(()=>{
+console.log('test')
+    document.getElementById('myplayer').mediaPlayer({
+      autoplay: false,
+      src: dymamicSrc
+    })
+
+  hlsPlayer()
+})
+
+
+</script>
+
+<script>        
+document.getElementById("myplayer").mediaPlayer({
+        autoplay : false,
+        src : "samples-data/examples/vid/amalia01.mp4",
+});
+</script>
--- components/molecules/MoleculeAnnotationLeftPanel.vue
+++ components/molecules/MoleculeAnnotationLeftPanel.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="col-span-3 bg-surface-700 px-5 py-5 h-full max-h-full xs:max-h-[28%] overflow-auto">
     <AtomVideoHls ref="AtomVideoHlsRef" :data="data" :videoSrc="videoSrc" :locals="locals" @timecode-update="emits('scroll-to-segment',$event)" />
+    <AtomVideoAmalia :videoSrc="videoSrc" />
     <AtomTopicList :colors="colors" />
     <h2 class="text-white text-3xl md:block xs:hidden p-3 font-semibold">Segmentation</h2>
       <p class="text-white p-3 md:block xs:hidden ">
@@ -19,6 +20,7 @@
 <script setup>
   import AtomVideoHls from '../atoms/AtomVideoHls'
   import AtomTopicList from '../atoms/AtomTopicList'
+  import AtomVideoAmalia from '../atoms/AtomVideoAmalia.vue';
 
 const props = defineProps({
   data: null,
