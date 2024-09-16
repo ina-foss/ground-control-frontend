abb3b11d337fe07df0c337a57e8ca7a58a91540e
belkays <belkays@ina.fr>
2024-09-16 13:45:20 +0200

    debug auth

--- pages/auth.vue
+++ pages/auth.vue
@@ -2,26 +2,35 @@
   <div class="mb-6 text-blue-700 text-2xl">
     Loading authentication...
   </div>
-
 </template>
 
 <script setup>
-
 import { useService } from "@/composables/useService";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 
-
-const services = useService()
-const router = useRouter()
+const services = useService();
+const router = useRouter();
 
 const authenticateOidc = async () => {
-
   try {
-    await services.$auth.signInCallback()
-    router.push("/")
-  } catch (error) {
-    console.error(error)
-  }
-}
+    if (window.location.search.search('state=') !== -1) {
+      console.log("state not found, callback");
 
-await authenticateOidc()
+      await services.$auth.signInCallback();
+
+      router.push("/");
+    }
+
+
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Use onMounted to handle async logic
+onMounted(async () => {
+  await authenticateOidc();
+});
 </script>
