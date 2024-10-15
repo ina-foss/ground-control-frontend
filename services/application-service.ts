18307c65015878f8b5ba61dfefa7d11e22e700b1
Pablo <pratouit-prestataire@ina.fr>
2024-10-15 11:39:35 +0200

    add color related service

--- services/application-service.ts
+++ services/application-service.ts
@@ -24,6 +24,20 @@ export default class ApplicationService {
     return { Authorization: `Bearer ${access_token.value}` };
   }
 
+  public computeColor= (seed )=>{
+    let color ='--extra-'+ (seed%10+1)
+    let hex = getComputedStyle(document.body).getPropertyValue(color)
+    return { color: color, hex: hex }
+  }
+
+  public textColorPicker(bgColor) {
+    let color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    let r = parseInt(color.substring(0, 2), 16); // hexToR
+    let g = parseInt(color.substring(2, 4), 16); // hexToG
+    let b = parseInt(color.substring(4, 6), 16); // hexToB
+    return ((r * 0.299) + (g * 0.587) + (b * 0.114)) <= 120 ? 'white' : 'black'
+  }
+
   public async checkUser() {
     const { userEmail, user } = storeToRefs(this.authStore);
 
