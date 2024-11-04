e1c04e62e4d36b6490d00799f290a3b40f305325
Pablo <pratouit-prestataire@ina.fr>
2024-11-04 12:06:45 +0100

    configure theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 import path from 'path';
+import Lara from '@primevue/themes/lara'
+import {definePreset} from '@primevue/themes'
+import { componentNames } from '#build/components';
+
+const MyPreset = definePreset(Lara, {
+  semantic:{
+    primary:{
+      50: 'rgb(var(--primary-50))',
+      100: 'rgb(var(--primary-100))',
+      200: 'rgb(var(--primary-200))',
+      300: 'rgb(var(--primary-300))',
+      400: 'rgb(var(--primary-400))',
+      500: 'var(--primary-color)',
+      600: 'rgb(var(--primary-600))',
+      700: 'rgb(var(--primary-700))',
+      800: 'rgb(var(--primary-800))',
+      900: 'rgb(var(--primary-900))',
+      950: 'rgb(var(--primary-950))',
+    },
+  },
+  components:{
+    button: {
+      colorScheme: {
+        light: {
+          outlinedPrimaryBorderColor: '{primary.color}',
+        }
+      }
+    },
+    datatable:{
+      colorScheme: {
+        light: {
+          rowHoverBackground: '{surface.50}',
+        }
+      }
+    },
+  }
+})
 
 export default defineNuxtConfig({
     ssr: false,
@@ -24,18 +61,21 @@ export default defineNuxtConfig({
         public: {
         }
     },
-    modules: ['./node_modules/@vue-macros/nuxt','@nuxtjs/tailwindcss','@nuxtjs/color-mode','nuxt-primevue', '@pinia/nuxt',"nuxt-lodash",'@nuxt/eslint'],
+    modules: ['./node_modules/@vue-macros/nuxt','@nuxtjs/tailwindcss','@nuxtjs/color-mode','@primevue/nuxt-module', '@pinia/nuxt',"nuxt-lodash",'@nuxt/eslint'],
     colorMode: {
       classSuffix:'',
     },
-      primevue: {
-        options: {
-            unstyled: true
-        },
-        importPT: {from: path.resolve(__dirname, './presets/lara/')},      //import and apply preset
-        components: {
-            include: '*'
-        }
+    primevue: {
+      options: {
+          theme: {
+            preset: MyPreset,
+            options: {
+            }
+          }
+      },
+      components: {
+          include: '*'
+      }
     },
     postcss: {
         plugins: {
