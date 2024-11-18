063a70da2e5523294bc77287e424bcd13b8c11c4
Pablo <pratouit-prestataire@ina.fr>
2024-11-18 16:42:09 +0100

    lint fixes

--- components/molecules/MoleculeSpan.vue
+++ components/molecules/MoleculeSpan.vue
@@ -1,22 +1,21 @@
 <template>
   <div :class="` col-span-4 flex flex-col overflow-y-auto     `">
     <div class=" h-[33px] mt-2 flex justify-center gap-10 sticky top-0">
-      <SelectButton multiple v-model="labelSelected" class="  " :options="labels" aria-labelledby="basic" />
+      <SelectButton v-model="labelSelected" multiple class="  " :options="labels" aria-labelledby="basic" />
       <div class="flex overflow-visible gap-1 items-center">
         <InputText v-model="newLabel" class="h-full  " />
         <Button icon="pi pi-plus" @click="addLabel()" />
       </div>
     </div>
     <div v-if="options.bloc">
-      <AtomTranscriptionSpan v-for="(local, index) in locals" :local="local" @mouseup="handleSelection" :key="index"  />
+      <AtomTranscriptionSpan v-for="(local, index) in locals" :key="index" :local="local" @mouseup="handleSelection"  />
     </div>
     <div v-else>
       <div
-v-for="word in aggregatedLocals" :key="word.tcin" :tcin="unixToTimestamp(word.tcin)" v-html="word.data.text[0]"
-        :tcout="unixToTimestamp(word.tcout)" :class="`inline  ${_.find(['.', ','], (char) => char == word.data.text[0]) ? 'pl-0' : 'pl-1'} hover:bg-surface-200`"
-        @mouseup="handleSelection">
-
-      </div>
+v-for="word in aggregatedLocals" :key="word.tcin" :tcin="unixToTimestamp(word.tcin)" :tcout="unixToTimestamp(word.tcout)"
+        :class="`inline  ${_.find(['.', ','], (char) => char == word.data.text[0]) ? 'pl-0' : 'pl-1'} hover:bg-surface-200`" @mouseup="handleSelection"
+         />
+      {{ word.data.text[0]}}
     </div>
   </div>
     <div class=" h-[80%] top-[20%] flex flex-col items-center place-content-center  gap-10 col-span-2">
@@ -52,7 +51,6 @@ v-for="word in aggregatedLocals" :key="word.tcin" :tcin="unixToTimestamp(word.tc
     bloc: true
   })
 
-  const emits = defineEmits([ 'on-segment-click' ]);
 
   const locals = defineModel<Array>('locals')
 
@@ -142,7 +140,6 @@ const onDeleteSpan = ({ index } : { index : number }) => {
 
 watch(()=>labelSelected.value,(newLabel:any)=>{
   if(typeof currentFocus != 'undefined'){
-      const span = spanRefArray.value[currentFocus]
       spanRefArray.value[currentFocus].label = newLabel
   }
 },{immediate: true})
@@ -247,11 +244,11 @@ const handleSelection = (spanArg: any) => {
         fragment.appendChild(node)
       });
         if (docFragment.firstChild?.firstChild?.nodeType == 1){
-          let blocs =  docFragment.childNodes
+          const blocs =  docFragment.childNodes
           const border = docFragment.firstChild.cloneNode(false)
           const blocNb = blocs.length
           blocs.forEach((previousBlock,index)=>{
-            let wordArray = previousBlock.childNodes
+            const wordArray = previousBlock.childNodes
             if(index==0){
               wordArray.forEach((word)=>{
               docFragment.appendChild(word.cloneNode(true))
@@ -339,9 +336,9 @@ const handleSelection = (spanArg: any) => {
   }
 
   function formatRelation(relationArg: never): any {
-    let relation = {}
+    const relation = {}
     relation.property = []
-    let property = {}
+    const property = {}
     property.key = 'relationType'
     property.value = 'Indiciates'
     relation.property.push(property)
