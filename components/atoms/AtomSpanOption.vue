017b84beaba6c7831b7ee0b337f97a4ac4ba51e6
Pablo <pratouit-prestataire@ina.fr>
2024-11-20 14:34:17 +0100

    control tc and bloc toggles

--- components/atoms/AtomSpanOption.vue
+++ components/atoms/AtomSpanOption.vue
@@ -18,10 +18,31 @@
 
 
 <script setup lang="ts">
+import { useToast } from '#imports';
 
 const span = defineModel<boolean>('span')
 const timecode = defineModel<boolean>('timecode')
 const bloc = defineModel<boolean>('bloc')
+
+const toast = useToast()
+
+let previousTimecode
+
+watch(()=>bloc.value,(value)=>{
+  if (!value){
+    previousTimecode = markRaw(timecode.value)
+    timecode.value = false
+  }
+  else{
+    timecode.value = previousTimecode
+  }
+},{immediate: true})
+
+watch(()=>timecode.value,(value)=>{
+  if(!bloc.value && value == true){
+    toast.add({detail: "Impossible de visualiser les timecodes lorsque l'option bloc est desactivée", summary: 'Erreur', severity: 'error', life: 3000})
+    timecode.value = false }
+})
 
 
 
