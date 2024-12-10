efaa8fdbdb7284c59026c144d86da5d1e47a4e1e
Pablo <pratouit-prestataire@ina.fr>
2024-12-10 15:34:11 +0100

    change setup.ts

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,12 @@
 import { beforeAll } from 'vitest';
 import { setup } from '@nuxt/test-utils';
+import path from 'path';
 
 beforeAll(async () => {
-    await setup({
-        server: true,
-        browser: true,
-    });
+
+  await setup({
+    server: true,
+    browser: true,
+    rootDir: path.resolve(__dirname, '../'), // Adjust the path as needed
+  });
 });
