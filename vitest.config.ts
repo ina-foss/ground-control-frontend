b72007cbb74cdd6b00dcb2eb7df290f80eb27e7b
Pablo <pratouit-prestataire@ina.fr>
2024-12-10 14:40:00 +0100

    disable vite watch

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
     test:{
+        watch: false,
         environment:'nuxt',
         environmentMatchGlobs:[
             ['tests', 'jsdom']
