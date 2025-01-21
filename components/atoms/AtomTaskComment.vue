01653c9a40bd2fb5f4df60fc4830b38c8229c7fa
Pablo <pratouit-prestataire@ina.fr>
2025-01-21 11:34:21 +0100

    rename comment title

--- components/atoms/AtomTaskComment.vue
+++ components/atoms/AtomTaskComment.vue
@@ -4,7 +4,7 @@
       <div class=" rounded-lg w-[250px]">
         <Accordion class="w-full rounded" >
           <AccordionPanel>
-            <AccordionHeader class="!bg-white hover:!bg-white rounded ">Commentaire</AccordionHeader>
+            <AccordionHeader class="!bg-white hover:!bg-white rounded ">Commentaires</AccordionHeader>
             <AccordionContent>
               <div class="flex flex-col gap-[10px] text-title">        <!--List Messages-->
                 <div v-if="comments?.length != 0" :items="comments" :itemSize="50" class="mb-3 border border-surface-200 dark:border-surface-700 rounded " style="height:125px; overflow-y: auto">
