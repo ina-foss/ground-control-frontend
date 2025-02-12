f81d1de8a606cd44ab2acb8c3ceab60beb7ff4fe
Pablo <pratouit-prestataire@ina.fr>
2025-02-12 16:17:35 +0100

    Fix: ProgressBar handle ignored topics

--- components/atoms/AtomProgressBar.vue
+++ components/atoms/AtomProgressBar.vue
@@ -4,7 +4,7 @@
       class="bg-gradient-to-t flex flex-col overflow-hidden from-surface-200 to-surface-200 h-full w-7 rounded-lg">
       <div
         v-for="(group, index) in groupedTopics" :key="index" v-tooltip="preventDefaultTitle(topicList[parseInt(group.topic)])"
-        class="cursor-pointer w-full h-[--length] bg-[--color] hover:opacity-50" :style="`--color:${colors[parseInt(group.topic)]}; --length:${((100 / totalLength) * group.count) + '%'}`"
+        :class="{ 'cursor-pointer': topicList[parseInt(group.topic)] != undefined,  'w-full h-[--length] bg-[--color] hover:opacity-50': true }" :style="`--color:${colors[parseInt(group.topic)]}; --length:${((100 / totalLength) * group.count) + '%'}`"
         @click="$emit('progressBarJump',{topic: group.topic})"/>
 
     </div>
@@ -35,7 +35,8 @@ topics:{
 defineEmits(['progressBarJump']);
 
 function preventDefaultTitle(topic){
-    if( !topic?.title ) return "Topic "+ topic?.id
+    if (!topic) return null
+    if( !topic?.title) return "Topic "+ topic?.id
     return topic?.title
   }
 
