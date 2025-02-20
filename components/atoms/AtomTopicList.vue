71a4679ae38406b79d35e4dc8f752ef83ebde7bd
Pablo <pratouit-prestataire@ina.fr>
2025-02-20 14:35:43 +0100

    remove Accordeon of TopicList

--- components/atoms/AtomTopicList.vue
+++ components/atoms/AtomTopicList.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class=" rounded-lg w-full gap-3  h-full">
-    <Accordion  class="w-full rounded  h-full" >
-      <AccordionPanel class="w-full rounded  !h-full">
-        <AccordionHeader class="!bg-white hover:!bg-white rounded !border-t-transparent !border-l-transparent !border-r-transparent ">Topics</AccordionHeader>
-        <AccordionContent  class="w-full rounded  !h-full !border-transparent ">
-          <div class="flex flex-col gap-[10px] text-title  !h-full">
-            <div class="flex  h-full">
-              <div v-if="cleanedColors?.length != 0" :items="cleanedColors" :itemSize="50" class="w-full h-full" >
+  <div class=" rounded-b-md  w-full gap-3 bg-white h-full ">
+          <div class="flex flex-col gap-[10px] text-title p-lg  !h-full">
+              <div v-if="cleanedColors?.length != 0" :items="cleanedColors" :itemSize="50" class="w-full h-full  " >
                 <ScrollPanel class="h-full" :dt="{
       bar : {
         background: 'var(--primary-color)',
@@ -16,7 +11,7 @@
         style : 'right: -10px;'
         }
     }">
-                <div v-for="(color, index) in cleanedColors" :key="index"  >
+                <div v-for="(color, index) in cleanedColors" :key="index"   >
                   <div class="flex pb-2 items-start gap-2 w-full overflow-visible">
                       <div v-if="index != 0" class="flex h-8 items-center hover:bg-neutral   hover:cursor-pointer overflow-visible w-full " @click="emits('topicClick',{topic: index})" >
                         <div :style="`background-color: ${color}40; border: solid 3px ${color}80; line-height: 10px `" class="min-w-4 h-4 place-content-center relative rounded-full flex  font-bold overflow-visible justify-center z-10 items-center text-white text-[10px] ">
@@ -26,12 +21,8 @@
                   </div>
                 </div>
                 </ScrollPanel>
-              </div>
             </div>
           </div>
-        </AccordionContent>
-      </AccordionPanel>
-    </Accordion>
   </div>
 </template>
 <script setup lang="js">
