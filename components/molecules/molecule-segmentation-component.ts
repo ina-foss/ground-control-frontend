917f8c8866a99e082f2683f27f6873719bd86534
Pablo <pratouit-prestataire@ina.fr>
2025-03-13 14:33:46 +0100

    feat: change the topic id generation method

--- components/molecules/molecule-segmentation-component.ts
+++ components/molecules/molecule-segmentation-component.ts
@@ -119,7 +119,7 @@ export default defineComponent({
 
     const activateTopic = ({ index }:{index: number})=>{
         let currentIndex = index
-        const topic = newTopic()
+        const topic = generateTopicNumber()
         createTopic({ id: topic, labels: [] })
         do {
           topics[currentIndex] = topic
@@ -131,7 +131,7 @@ export default defineComponent({
 
     const createBreak = (index: number) => {
       let currentIndex = index
-      const topic = newTopic()
+      const topic = generateTopicNumber()
       const topTopic = topics[currentIndex]
       createTopic({ id: topic, labels: [] })
       copyTopicData(topTopic,topic)
@@ -154,21 +154,14 @@ export default defineComponent({
     }
 
 
-    const newTopic = (): number => {
-      let result
-      colors.forEach((color, index) => {
-        if (parseInt(index) != 0 && _.findIndex(topics, (el) => el == parseInt(index)) == -1) {
-          result = parseInt(index)
-        }
-      })
-      if (!result) {
-        result = _.max(topics) + 1 || 1
-        const randomColor = computeColor(result).hex
-        colors.push(randomColor)
-      }
+    const generateTopicNumber = (): number => {
+      let result = _.max(topics) + 1 || 1
+      const randomColor = computeColor(result).hex
+      colors.push(randomColor)
       return result
 
     }
+
 
     const loadTopics = () => {
       const max = _.maxBy(locals, (local) => local?.data?.topic) // Search for maximum topic number
