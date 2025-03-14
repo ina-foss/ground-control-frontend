9f9a7b82b6bc4bf18b21893faf989fa03bd3cc33
Pablo <pratouit-prestataire@ina.fr>
2025-03-14 10:26:59 +0100

    fix: apply style with dedicated function

--- components/atoms/topicList/AtomTopicList.vue
+++ components/atoms/topicList/AtomTopicList.vue
@@ -15,7 +15,8 @@
             <div class="flex pb-2 items-start gap-2 w-full overflow-visible">
               <div class="flex h-8 items-center hover:bg-neutral   hover:cursor-pointer overflow-visible w-full "
                 @click="emit('topicClick', { topic: parseInt(index) })">
-                <div :style="`background-color: ${color}40; border: solid 3px ${color}80; line-height: 10px `"
+                <!-- TODO: Changer la façon d'appliquer le style pour fix le bug avec le topix 3 -->
+                <div :style="styleTopicCircle(computeColor(parseInt(index)).hex)"
                   class="min-w-4 h-4 place-content-center relative rounded-full flex  font-bold overflow-visible justify-center z-10 items-center text-white text-[10px] " />
                 <h2 class="px-2 shrink  font-semibold h-fit overflow-hidden text-ellipsis line-clamp-2 "
                   style="line-height: 16px;">{{ preventDefaultTitle(topicList[parseInt(index)]) }}</h2>
