923c3a73e3aa5c4152028a81d2b4b348b27a9596
Pablo <pratouit-prestataire@ina.fr>
2025-12-05 12:14:55 +0100

    🏷️ update Span types

--- types/span.d.ts
+++ types/span.d.ts
@@ -1,17 +1,33 @@
-declare interface Span {
-    id: number,
-    label: string,
+declare interface SpanBase {
+  /**
+   * Unique identifier across spans (virtual included) and groups
+   * @readonly
+   */
+  id: number,
+  /**
+   *  String that will be display to the user
+   *
+   *  May contains span text content or plugin value
+   */
+  label: string,
+}
+
+declare interface Span extends SpanBase {
     deletedItems: number,
+    /**
+     * Text extracted from {@link nodes}
+     *
+     * Note: the value is updated only when the user saves his annotation
+     */
     text? : string,
-    nodes: Array<Node>,
+    /** Contains all the `Node` element the user selected to create the span */
+    nodes: Node[],
     tcin: number | string,
     tcout: number | string,
     plugins: Array
   }
 
-declare interface SpanGroup  {
-    id: number,
-    label: string,
+declare interface SpanGroup extends spanBase  {
     spans: [
       {
         spanId: string | number,
@@ -26,9 +42,7 @@ declare interface SpanGroup  {
     tcout: number | string,
   }
 
-declare interface VirtualSpan  {
-    id: number,
-    label: string,
+declare interface VirtualSpan extends Base {
     deletedItems?: number,
     plugins: Array
 }
