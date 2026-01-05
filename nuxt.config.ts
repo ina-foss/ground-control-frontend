4b66eede94b954c633bcd43012b65173ba0f15a2
mjelassi <mjelassi-prestataire@ina.fr>
2026-01-05 11:37:38 +0100

    fix : remove unused files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -193,9 +193,7 @@ export default defineNuxtConfig({
     locales: [
       { code: 'fr', iso: 'fr-FR', name: 'Français (France)',file: 'fr.json'},
       { code: 'en', iso: 'en-US', name: 'English',file: 'en.json'}
-    ],
-
-    vueI18n: './i18n.config.ts'
+    ]
   }
 
 })
